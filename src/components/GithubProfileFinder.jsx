07ea72fcb9c1d33088e8d0ddb031c051682e99c0
Keerthana <keerthiram2612@gmail.com>
2024-07-23 15:55:23 +0530

    adding the Api key of the github

---
+++ src/components/GithubProfileFinder.jsx
@@ -0,0 +1,52 @@
+import "./Githubstyle.css"
+import React, { useState } from 'react'
+import { useEffect } from "react";
+import User from "./user";
+
+export default function GithubProfileFinder(){
+  const [userName, setUserName] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchGithubUserData() {
+    setLoading(true);
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+    if (data) {
+      setUserData(data);
+      setLoading(false);
+      setUserName('')
+    }
+  }
+
+  function handleSubmit() {
+    fetchGithubUserData()
+  }
+
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading data ! Please wait</h1>;
+  }
+
+  return (
+    <div className="github-profile-container">
+      <div className="input">
+      <div className="input-wrapper">
+        <input
+          name="search-by-username"
+          type="text"
+          placeholder="Search Github Username..."
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {userData !== null ? <User user={userData} /> : null}
+      </div>
+    </div>
+  );
+}
--- src/routes/Finder.js
+++ src/routes/Finder.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import GithubProfile from "../components/GithubProfile"
+import GithubProfileFinder from '../components/GithubProfileFinder'
 
 export default function Project() {
   return (
     <div>
       <Navbar/>
-      <GithubProfile/>
+       <GithubProfileFinder/>
        <Footer/>
     </div>
   )
